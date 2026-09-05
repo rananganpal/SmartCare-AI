5560207536a15f046e210c5ef7974a90639421cd
Ranangan Pal <rananaganpal@gmail.com>
2026-09-05 20:24:24 +0530

    Update API URL to HTTPS

--- backend/main.py
+++ backend/main.py
@@ -41,7 +41,7 @@ app.add_middleware(
 
         # Local FastAPI testing
         "http://localhost:8000",
-        "http://127.0.0.1:8000"
+        "https://smartcare-ai-oqq2.onrender.com"
     ],
 
     allow_credentials=False,
